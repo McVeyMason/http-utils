461466262a557b6dda4bcfcbc15312fb31af8ece
Mason McVey <mason.mcvey@columbia.edu>
2024-10-12 15:42:30 +0000

    Finished decoding section:  - uri_decode and uri_decode_l  - Added test cases for each.

--- http_utils.c
+++ http_utils.c
@@ -25,8 +25,6 @@ int decode_uri(const char* str, char **res)
             continue;
         }
         if (strlen(curr_str) < 3) {
-            *curr_res = '%';
-            curr_res++;
             curr_str++;
             errnum++; 
             continue;
@@ -44,22 +42,74 @@ int decode_uri(const char* str, char **res)
         curr_res++;
         curr_str += 3;
     }
-    curr_res = 0;
+    *curr_res = 0;
     *res = realloc(result, (strlen(result) + 1) *sizeof(char));
     return errnum;
 }
 
 int decode_uri_l(const char* str, char **res, int len)
 {
-
-
-    return 0;
+    char *result = malloc((len + 1) * sizeof(char));
+    *res = result;
+    if (result == NULL) {
+        perror("Malloc Failed");
+        return -1;
+    }
+    const char *curr_str = str;
+    char *curr_res = *res;
+    int errnum = 0;
+    while (*curr_str && curr_str < str + len) {
+        if (*curr_str != '%') {
+            *curr_res = *curr_str;
+            curr_str++;
+            curr_res++;
+            continue;
+        }
+        if (strlen(curr_str) < 3 || curr_str +3 > str + len) {
+            curr_str++;
+            errnum++; 
+            continue;
+        }
+        unsigned int charnum;
+        sscanf(curr_str+1, "%2X", &charnum);
+        if (charnum == 0)
+            sscanf(curr_str+1, "%2x", &charnum);
+        if (charnum == 0) {
+            errnum++;
+            curr_str++;
+            continue;
+        }
+        *curr_res = (char) charnum;
+        curr_res++;
+        curr_str += 3;
+    }
+    *curr_res = 0;
+    *res = realloc(result, (strlen(result) + 1) *sizeof(char));
+    return errnum;
 }
 
 int encode_uri(const char* str, char **res)
 {
+    char *result = malloc((strlen(str)* 3 + 1)* sizeof(char));
+    *res = result; 
+    if (result == NULL) {
+        perror("malloc failed on encode_uri");
+        return -1;
+    }
 
-
+    char *curr_str = str;
+    char *curr_res = result;
+    while (*curr_str) {
+        if (    ('a' <= *curr_str && *curr_str <= 'z') ||
+                ('A' <= *curr_str && *curr_str <= 'Z') ||
+                ('0' <= *curr_str && *curr_str <= '9')) {
+            *curr_res = *curr_str;
+            curr_str++;
+            curr_res++;
+            continue;
+        }
+    }
+    *res = realloc(result, (strlen(result) + 1) * sizeof(char));
     return 0;
 }
 
--- utils-test.c
+++ utils-test.c
@@ -1,8 +1,58 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 
 #include "http_utils.h"
 
+void test_decode_uri(int *passed, int *total, const char *input, const char *output)
+{
+    char *result;
+    (*total)++;
+    int error = decode_uri(input, &result);
+    if (!error) {
+        if (!strcmp(result, output)) 
+            (*passed)++;
+        else
+            fprintf(stderr, "Test case %d FAILED with result \"%s\", should be \"%s\"\n", *total,  result, output);
+    }
+    else { 
+        if (!strcmp(result, output)) {
+            (*passed)++;
+            fprintf(stderr, "Test case %d PASSED with error, input was %s, errno was %d\n", 
+                    *total, input, error);
+        }
+        else
+            fprintf(stderr, "Test case %d FAILED with result \"%s\", should be \"%s\" and error %d\n", 
+                    *total,  result, output, error);
+    }
+    free(result);
+}
+
+void test_decode_uri_l(int *passed, int *total, const char *input, int len, const char *output)
+{
+    char *result;
+    (*total)++;
+    int error = decode_uri_l(input, &result, len);
+    if (!error) {
+        if (!strcmp(result, output)) 
+            (*passed)++;
+        else
+            fprintf(stderr, "Test case %d FAILED with result \"%s\", should be \"%s\"\n", *total,  result, output);
+    }
+    else { 
+        if (!strcmp(result, output)) {
+            (*passed)++;
+            fprintf(stderr, "Test case %d PASSED with error, input was %s, errno was %d\n", 
+                    *total, input, error);
+        }
+        else
+            fprintf(stderr, "Test case %d FAILED with result \"%s\", should be \"%s\" and error %d\n", 
+                    *total,  result, output, error);
+    }
+    free(result);
+}
 
 int main(int argc, char **argv)
 {
@@ -12,6 +62,48 @@ int main(int argc, char **argv)
         fprintf(stdout, "Decoded %s to %s with error code %d\n", argv[1], 
                 result, error);
         free(result);
+        error = decode_uri_l(argv[1], &result, 6);
+        fprintf(stdout, "Decoded %s to %s with error code %d\n", argv[1], 
+                result, error);
+        free(result);
     }
+
+    /**
+     * Unit tests
+     */
+    int passed = 0;
+    int total = 0;
+    fprintf(stderr, "------BEGIN decode_uri UNIT TEST------\n");
+
+    test_decode_uri(&passed, &total, "", "");
+    test_decode_uri(&passed, &total, "Hello World", "Hello World");
+    test_decode_uri(&passed, &total, "%25", "%");
+    test_decode_uri(&passed, &total, "A%25", "A%");
+    test_decode_uri(&passed, &total, "%25HI", "%HI");
+    test_decode_uri(&passed, &total, "%2", "2");
+    test_decode_uri(&passed, &total, "%00", "00");
+
+    fprintf(stderr, "------END UNIT TEST------\n"
+            "Passed %d of %d cases\n", passed, total);
+    
+    passed = 0;
+    total = 0;
+
+    fprintf(stderr, "------BEGIN decode_uri_l UNIT TEST------\n");
+
+    test_decode_uri_l(&passed, &total, "", 1, "");
+    test_decode_uri_l(&passed, &total, "Hello World", 5, "Hello");
+    test_decode_uri_l(&passed, &total, "%25",3, "%");
+    test_decode_uri_l(&passed, &total, "A%25",4, "A%");
+    test_decode_uri_l(&passed, &total, "%25HI",3, "%");
+    test_decode_uri_l(&passed, &total, "%2",3, "2");
+    test_decode_uri_l(&passed, &total, "%00",1, "");
+    test_decode_uri_l(&passed, &total, "%00",2, "0");
+    test_decode_uri_l(&passed, &total, "%00",3, "00");
+
+    fprintf(stderr, "------END UNIT TEST------\n"
+            "Passed %d of %d cases\n", passed, total);
+ 
+
     return 0;
 }
